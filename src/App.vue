e65afacc15c94a03c03354883345c0f1c0af9757
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-27 12:48:59 -0500

    Dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <the-header />
-    <badge-list />
+    <!-- <badge-list />
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -9,22 +9,31 @@
     ></user-info>
     <course-goals #default="slotProps">
       <h2>{{ slotProps.item }}</h2>
-    </course-goals>
+    </course-goals> -->
+
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
-import BadgeList from './components/BadgeList.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 import TheHeader from './components/TheHeader.vue';
-import UserInfo from './components/UserInfo.vue';
+// import UserInfo from './components/UserInfo.vue';
 
 export default {
   components: {
-    BadgeList,
-    CourseGoals,
+    // BadgeList,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
     TheHeader,
-    UserInfo,
+    // UserInfo,
   },
   data() {
     return {
@@ -33,7 +42,13 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      selectedComponent: 'active-goals',
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
