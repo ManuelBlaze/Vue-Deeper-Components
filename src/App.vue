69a6eda9b3aa77b042a70dccebebb8f7dd5eace0
Manuel Escobar <manuel.escobar1217@gmail.com>
2022-01-27 10:25:35 -0500

    Adjust local components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
+    <the-header />
+    <badge-list />
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -11,7 +11,16 @@
 </template>
 
 <script>
+import BadgeList from './components/BadgeList.vue';
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    BadgeList,
+    TheHeader,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
